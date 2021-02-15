0cde5b4a685f2d42bb966563c34d8e71f1c2d8ea
YanConigliaro <42974479+YanConigliaro@users.noreply.github.com>
2021-02-15 13:20:04 +0100

    Voir l'incidence sur un graph chartjs #7 (#16)
    * Voir l'incidence sur un graph chartjs #7
    
    Co-authored-by: Ruben Houri <ruben.houri@etu.unice.fr>

---
+++ client/web-client/src/components/ChartIncidences.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useRef, useState } from 'react';
+import Chartjs from 'chart.js';
+import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+
+const useStyles = makeStyles(() => ({
+  chartContainer: {
+    height: '40vh',
+    width: '80vw',
+  },
+}));
+
+export const ChartIncidences = ({ data }) => {
+  const chartContainer = useRef(null);
+  const [, setChartInstance] = useState(null);
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    const newChartInstance = new Chartjs(chartContainer.current, {
+      type: 'line',
+      data: {
+        labels: data.map((d) => d.jour),
+        datasets: [
+          {
+            label: 'Incidences',
+
+            data: data.map((d) => d.tx_std),
+          },
+        ],
+      },
+    });
+    setChartInstance(newChartInstance);
+  }, [chartContainer]);
+
+  return (
+    <div className={classes.chartContainer}>
+      <canvas ref={chartContainer} />
+    </div>
+  );
+};
+
+ChartIncidences.defaultProps = {
+  data: [],
+};
+
+ChartIncidences.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      reg: PropTypes.string,
+      jour: PropTypes.string,
+      P_f: PropTypes.number,
+      P_h: PropTypes.number,
+      P: PropTypes.number,
+      pop_h: PropTypes.number,
+      pop_f: PropTypes.number,
+      pop: PropTypes.number,
+      cl_age90: PropTypes.number,
+      tx_std: PropTypes.number,
+    }),
+  ),
+};
