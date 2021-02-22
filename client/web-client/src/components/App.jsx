d21da81d83796b754f74229515aad21d3d2dfda5
davidbisegna <david.bisegna@etu.unice.fr>
2021-02-22 21:34:20 +0100

    [#13] Added theme saving + modal to propose dark theme

--- client/web-client/src/components/App.jsx
+++ client/web-client/src/components/App.jsx
@@ -2,13 +2,17 @@ import React, { useEffect } from 'react';
 import {
   AppBar,
   makeStyles, Tooltip, IconButton,
-  Badge, Typography,
+  Badge, Typography, Dialog,
+  DialogActions, DialogContent,
+  DialogContentText, DialogTitle,
+  Button,
 } from '@material-ui/core';
 import {
   green,
   blue,
   deepPurple,
 } from '@material-ui/core/colors';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Toolbar from '@material-ui/core/Toolbar';
 import { useGet } from 'restful-react';
 import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
@@ -69,18 +73,42 @@ function useStyles(theme) {
   }));
 }
 
+const initialThemeFromStorage = JSON.parse(localStorage.getItem('reactAppTheme'));
+let hasProposedDarkTheme = false;
+let prefersDarkMode;
+
 export const App = () => {
   const { data: numbercaseconfirm, loading, refetch } = useGet({ path: 'api/v0/case-confirm' });
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const [theme, setTheme] = React.useState(getTheme('blue', false));
-  const [currentColor, setCurrentColor] = React.useState('blue');
+  const [dialogIsOpen, setDialogIsOpen] = React.useState(false);
+  prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  let initialColor;
+  let initialDarkState;
+  let initialTheme;
+  if (initialThemeFromStorage !== null) {
+    initialColor = initialThemeFromStorage.color;
+    initialDarkState = initialThemeFromStorage.isDark;
+    initialTheme = getTheme(initialColor, initialDarkState);
+  } else {
+    initialColor = 'blue';
+    initialDarkState = false;
+    initialTheme = getTheme(initialColor, initialDarkState);
+  }
+  const [currentColor, setCurrentColor] = React.useState(initialColor);
   const [state, setState] = React.useState({
-    isDarkTheme: false,
+    isDarkTheme: initialDarkState,
   });
+  const [theme, setTheme] = React.useState(initialTheme);
+
+  const changeTheme = (newColor, newIsDark) => {
+    setTheme(getTheme(newColor, newIsDark));
+    localStorage.setItem('reactAppTheme', JSON.stringify({ color: newColor, isDark: newIsDark }));
+  };
+
   const classes = useStyles(theme)();
   const handleChange = (event) => {
     setState({ ...state, [event.target.name]: event.target.checked });
-    setTheme(getTheme(currentColor, event.target.checked));
+    changeTheme(currentColor, event.target.checked);
   };
 
   const handleClick = (event) => {
@@ -91,16 +119,32 @@ export const App = () => {
     setAnchorEl(null);
   };
 
+  const handleCloseDialog = (isActivatingDarkMode) => {
+    setDialogIsOpen(false);
+    if (!isActivatingDarkMode) {
+      localStorage.setItem('hasRefusedDarkTheme', true);
+    }
+    setState({ ...state, isDarkTheme: isActivatingDarkMode });
+    changeTheme(currentColor, isActivatingDarkMode);
+  };
+
   const MAX_PRINTABLE_NUMBER = 99999999;
   const INTERVAL_TIME = 60000;
 
   useEffect(() => {
-    setTheme(getTheme(currentColor, state.isDarkTheme));
+    if (!hasProposedDarkTheme && !state.isDarkTheme) {
+      const hasRefusedDarkTheme = localStorage.getItem('hasRefusedDarkTheme');
+      if (prefersDarkMode && !hasRefusedDarkTheme) {
+        hasProposedDarkTheme = true;
+        setDialogIsOpen(true);
+      }
+    }
+    changeTheme(currentColor, state.isDarkTheme);
     const interval = setInterval(() => {
       refetch();
     }, INTERVAL_TIME);
     return () => clearInterval(interval);
-  }, [currentColor]);
+  }, [currentColor, prefersDarkMode]);
 
   let badge;
   if (loading) {
@@ -187,6 +231,28 @@ export const App = () => {
             </Popover>
           </Toolbar>
         </AppBar>
+        <Dialog
+          open={dialogIsOpen}
+          onClose={() => handleCloseDialog(false)}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">Utiliser le mode sombre ?</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Nous avons remarquer que vous utilisez le mode sombre sur votre ordinateur.
+              Voulez-vous activer le mode sombre ?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => handleCloseDialog(false)} color="primary">
+              Non
+            </Button>
+            <Button onClick={() => handleCloseDialog(true)} color="primary" autoFocus>
+              Oui
+            </Button>
+          </DialogActions>
+        </Dialog>
 
         <Switch>
           <Route exact path="/">
